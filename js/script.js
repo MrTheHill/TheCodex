54715abf50f0c935fb0ddb1ba6ce50a0367a9dbf
MrTheHill <harvey.hill@hotmail.co.uk>
2025-02-13 09:18:45 +0000

    dont remember lol

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
-document.getElementById('uploadedFile').addEventListener('change', onChange);
+// -------------------------------------------- V Loading from JSON V --------------------------------------------
+document.getElementById('uploadedFile').addEventListener('change', onChange); //Event listener for when a file gets uploaded
 
 function onChange(event) {
-    var reader = new FileReader();
+    
+    //Validate file type
+    const file = event.target.files[0];
+    if (!file || file.type !== "application/json") {
+        alert("Please upload a valid JSON file.");
+        return;
+    }
+
+
+    let reader = new FileReader();
     reader.onload = readerLoad;
-    reader.readAsText(event.target.files[0]);
+    reader.readAsText(file);
 }
 
 function readerLoad(event){
     console.log(event.target.result);
-    var obj = JSON.parse(event.target.result);
-    loadText(obj.data)
+    let obj = JSON.parse(event.target.result);
+    loadText(obj.data, "h2")
 }
 
-function loadText(file){
-    let heading = document.querySelector("h2");
-    heading.innerHTML = `<pre>${(JSON.stringify(file, null, 2)).replace(/[{},]/g, '')}</pre>`;
+function loadText(data, location){
+    let heading = document.querySelector(location);
+    heading.innerHTML = `<pre>${(JSON.stringify(data, null, 2)).replace(/[{},"]/g, '')}</pre>`;
 }
+
+// -------------------------------------------- V Loading from JSON V --------------------------------------------
